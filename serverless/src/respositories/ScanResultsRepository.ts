4dd24a6beb074d0025518c84e42197ff3da98bc6
Gokay Kucuk <serdargokaykucuk@gmail.com>
2020-03-20 22:17:59 +0100

    implemented create functionality

--- serverless/src/respositories/ScanResultsRepository.ts
+++ serverless/src/respositories/ScanResultsRepository.ts
@@ -1,10 +1,38 @@
+import { DynamoDB } from 'aws-sdk';
 
 type ScanResult = {
+    scanType: string,
     deviceId: string,
-    seendDeviceIds: Array<string>,
+    seenDeviceIds: Array<string>,
     timestamp: Date
+};
+
+type ScanResultAsParams = {
+    TableName: string,
+    Item:{
+        senderDeviceId: string,
+        seenDeviceId: string,
+        timeStamp: Date
+    }
 }
-export const Create = (scanResult: ScanResult) => {
-    
+
+export const CreateScanResult = async (scanResult: ScanResult) => {
+
+    const docClient = new DynamoDB.DocumentClient();
+
+    const scanType = scanResult.scanType === 'wifi' ? 'wifi' : 'bt';
+
+    for(const seenDeviceId of scanResult.seenDeviceIds){
+        const scanResultAsParams: ScanResultAsParams = {
+            TableName: process.env.DYNAMODB_TABLE_NAME as string,
+            Item: {
+                senderDeviceId: `${scanType}_${scanResult.deviceId}`,
+                seenDeviceId: `${scanType}_${seenDeviceId}`,
+                timeStamp: scanResult.timestamp
+            }
+        };
+        const putResult = await docClient.put(scanResultAsParams).promise();
+        console.log(putResult);
+    }
 };
 
--- serverless/tests/scanResultsRepository.test.ts
+++ serverless/tests/scanResultsRepository.test.ts
@@ -1,1 +1,13 @@
-test('can create scan results', )
+import {CreateScanResult} from "../src/respositories/ScanResultsRepository";
+
+test('can create scan results', async () => {
+    const testParams = {
+        scanType: "123123",
+        deviceId: "34454545",
+        seenDeviceIds: ["234234234","1231231","23424234"],
+        timestamp: new Date()
+    };
+
+    const scanResult = await CreateScanResult(testParams);
+    console.log(scanResult);
+});
